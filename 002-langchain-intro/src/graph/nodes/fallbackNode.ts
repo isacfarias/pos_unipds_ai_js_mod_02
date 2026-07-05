8a494a074b45f40a67482b4e8fdece9f13f2817b
Isac Farias <isac_farias@terceiros.sicredi.com.br>
2026-07-05 16:36:14 -0400

    add fallback

--- 002-langchain-intro/src/graph/nodes/fallbackNode.ts
+++ 002-langchain-intro/src/graph/nodes/fallbackNode.ts
@@ -1,16 +1,16 @@
-import { AIMessage } from "langchain";
+import { AIMessage, SystemMessage } from "langchain";
 import { type GraphState } from "../graph.ts";
 
 export function fallbackNode(state: GraphState): GraphState {
     const responseText = "Unknown command. Try 'make this uppercase' or 'convert to lowercase'"
-    const fallbackMessage = new AIMessage(responseText);
+    const fallbackMessage = new AIMessage(responseText).content.toString();
   
     return {
         ...state,
-        output: responseText,
+        output: fallbackMessage,
         messages: [
             ...state.messages,
-            fallbackMessage
+            new SystemMessage('Deu ruim!!')
         ] 
     }
 }
