38566076566cca52e16c7d1b2a6f739271b6e5fe
Isac Farias <isac_farias@terceiros.sicredi.com.br>
2026-08-08 21:13:42 -0400

    implementação de intenção

---
+++ 003-medical-appointment/src/services/llmService.ts
@@ -0,0 +1,63 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { config, type ModelConfig } from "../config.ts"
+import { z } from 'zod/v3'
+import { createAgent, HumanMessage, providerStrategy, SystemMessage } from "langchain";
+
+export class LLMService {
+    private config: ModelConfig
+    private llmClient: ChatOpenAI
+
+    constructor(configOverride?: ModelConfig) {
+        this.config = configOverride ?? config
+        this.llmClient = new ChatOpenAI({
+            apiKey: this.config.apiKey,
+            modelName: this.config.models.at(0),
+            temperature: this.config.temperature,
+            configuration: {
+                baseURL: 'https://openrouter.ai/api/v1',
+                defaultHeaders: {
+                    'HTTP-Referer': this.config.httpReferer,
+                    'X-Title': this.config.xTitle
+                }
+            },
+            //aqui vai a conf do open router(smart model)
+            modelKwargs: {
+                models: this.config.models,
+                provider: this.config.provider
+            }
+        })
+    }
+
+    async generateStructured<T>(
+        systemPrompt: string,
+        userPrompt: string,
+        schema: z.ZodSchema<T>
+    ) {
+        try {
+
+            const agent = createAgent({
+                model: this.llmClient,
+                tools: [],
+                responseFormat: providerStrategy(schema)
+            })
+
+            const messages = [
+                new SystemMessage(systemPrompt),
+                new HumanMessage(userPrompt)
+            ]
+
+            const resultData = await agent.invoke({ messages })
+            return {
+                sucess: true,
+                data: resultData.structuredResponse,
+            }
+        } catch (error) {
+            console.error('Error LLMService', error)
+            return {
+                sucess: true,
+                data: error instanceof Error ?
+                    error.message : String(error),
+            }
+        }
+    }
+}
